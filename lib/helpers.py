bc680c57de9de4d326f0be36309df17410f65f62
bururun <bururun@users.noreply.github.com>
2025-09-10 23:09:42 +0300

    improve: code refactoring

--- lib/helpers.py
+++ lib/helpers.py
@@ -7,3 +7,14 @@ def helper_function_2(x):
 def format_output(data):
     """Format output data."""
     return str(data).upper()
+
+
+# Helper functions
+
+def helper_function_15(x):
+    """Helper function for iteration 15."""
+    return x * 15
+
+def format_output(data):
+    """Format output data."""
+    return str(data).upper()
