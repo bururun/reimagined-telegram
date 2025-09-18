98e10da2b4b4a8b553fc078553c892e9947491c6
bururun <bururun@users.noreply.github.com>
2025-09-18 22:13:58 +0300

    docs(tests): add input sanitization

---
+++ lib/helpers.py
@@ -0,0 +1,9 @@
+# Helper functions
+
+def helper_function_2(x):
+    """Helper function for iteration 2."""
+    return x * 2
+
+def format_output(data):
+    """Format output data."""
+    return str(data).upper()
