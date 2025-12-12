31eae116566b34b76019fee99335f440f1e409fc
bururun <bururun@users.noreply.github.com>
2025-12-12 15:12:52 +0200

    ci: improve code structure

---
+++ tests/test_core.py
@@ -0,0 +1,10 @@
+# Tests for TelegramBot
+
+import unittest
+
+class TestCore(unittest.TestCase):
+    def test_basic(self):
+        self.assertEqual(1 + 1, 2)
+    
+    def test_function_7(self):
+        self.assertTrue(True)
